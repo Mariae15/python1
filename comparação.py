6c20b4334bade6842469b2fc82dd775930d9b080
Maria <maria.patricio.silva@escola.pr.gov.br>
2025-02-26 19:00:04 -0300

    Add files via upload

---
+++ comparação.py
@@ -0,0 +1,16 @@
+numero1 = 10
+numero2 = 10
+if(numero1 == numero2):
+    print("São números iguais")
+
+
+
+idade1 = 10
+idade2 = int("20")
+print(idade1 + idade2)
+
+
+
+nome = "Nico"
+sobrenome = "Steppat"
+print(nome, sobrenome)
