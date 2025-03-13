d805f27ddded0ca2b24bcfbc442132f720df562d
Maria <maria.patricio.silva@escola.pr.gov.br>
2025-03-13 08:57:31 -0300

    Update adivinhação.py

--- adivinhação.py
+++ adivinhação.py
@@ -1,14 +1,28 @@
+import random
+
 print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
-numero_secreto = 42
+numero_secreto = random.randrange(1,101)
 total_de_tentativas = 3
-rodada = 1
+
+print("Qual nível de dificudade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Defina o nível: "))
+
+if(nivel == 1):
+   total_de_tentativas = 20
+elif(nivel == 2):
+   total_de_tentativas = 10
+else:
+   (nivel == 3)
+   total_de_tentativas = 5
 
 for rodada in range (1, total_de_tentativas + 1):
    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-   chute_str = input("Digite o seu número: ")
+   chute_str = input("Digite um número entre 1 e 100:")
    print("Você digitou: ", chute_str)
    chute = int(chute_str)
 
